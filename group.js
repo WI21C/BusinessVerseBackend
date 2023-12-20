a4ec5609d0cf4b468885d425bbd334782203c502
Moritz <moritz.konrad.1@web.de>
2023-12-20 10:36:32 +0100

    item id gelöscht

--- group.js
+++ group.js
@@ -2,6 +2,6 @@ class Group{
     constructor(name, id, items) {
         this.name = name;
         this.id = id;
-        this.items = items.map(item => new Item(item.id, item.name, item.description, item.synonyms));
+        this.items = items.map(item => new Item(item.name, item.description, item.synonyms));
     }
 }
--- item.js
+++ item.js
@@ -1,6 +1,5 @@
 class Item {
-    constructor(id, name, description, synonyms) {
-        this.id = id; //id da name bei mehreren items gleich seien kann
+    constructor(name, description, synonyms) {
         this.name = name;
         this.description = description;
         this.synonyms = synonyms.map(syn => new Synonym(syn.name, syn.softwares, syn.args));
