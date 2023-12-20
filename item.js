793714aca5862652a0a2aa674c8cecea2a29186f
Moritz <moritz.konrad.1@web.de>
2023-12-20 10:31:25 +0100

    item id + group klasse hinzugefügt

--- item.js
+++ item.js
@@ -1,5 +1,6 @@
 class Item {
-    constructor(name, description, synonyms) {
+    constructor(id, name, description, synonyms) {
+        this.id = id; //id da name bei mehreren items gleich seien kann
         this.name = name;
         this.description = description;
         this.synonyms = synonyms.map(syn => new Synonym(syn.name, syn.softwares, syn.args));
